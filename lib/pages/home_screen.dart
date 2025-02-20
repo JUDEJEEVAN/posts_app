0ff0ca2d3385eeb37dfb34b18d976daaba02df4b
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2025-02-21 00:10:40 +0530

    home screen has been modified to show an icon which can lead the user to the create post screen.

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:posts_app/components/default_app_bar.dart';
 import 'package:posts_app/components/post_card.dart';
+import 'package:posts_app/pages/create_post_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,22 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xffF7F8F8),
       appBar: defaultAppBar(context, title: 'Posts'),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CreatePostScreen(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.history_edu,
+          color: Colors.blue.shade800,
+          size: 32,
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(16, 25, 16, 23),
         child: Column(
